757de7d64a8f24acb311a0aec967c8689bc8919f
andrea-bertaglia <andreabertaglia@gmail.com>
2024-03-22 16:09:54 +0100

    feat: js data input

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 // BONUS: Al posto del 21 finale, generate un numero intero random (tipo tra 0 e 99) 
 
-console.log("check link js OK");
+// Check collegamento files
+console.log("Hello World!");
+
+// Suddivisione
+// 1. INPUT DATI
+// 2. LOGICA
+// 3. OUTPUT
+
+// * 1. INPUT DATI
+// Richiesta del nome dell'utente
+let userName = prompt("Ciao! Scrivi il tuo NOME"); // string
+console.log(userName, "is:",typeof userName);
+
+// Richiesta del cognome dell'utente
+let userSurname = prompt("Ora scrivi il tuo COGNOME"); // string
+console.log(userSurname, "is:",typeof userSurname);
+
+// Richiesta del colore preferito dell'utente
+let userFavColor = prompt("Qual è il tuo colore preferito?"); // string
+console.log(userFavColor, "is:",typeof userFavColor);
