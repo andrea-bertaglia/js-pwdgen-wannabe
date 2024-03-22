d61587b7a56154a591a5da796c36cb8aa2b35597
andrea-bertaglia <andreabertaglia@gmail.com>
2024-03-22 16:00:02 +0100

    feat: project scaffolding

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Istruzioni
+// Chiedi all’utente tramite prompt il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+// BONUS: Al posto del 21 finale, generate un numero intero random (tipo tra 0 e 99) 
+
+console.log("check link js OK");
